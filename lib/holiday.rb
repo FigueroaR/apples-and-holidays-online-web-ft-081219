e052fa30bf3eb3cd34f781aef425c73b2c304228
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-19 22:04:33 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,7 +55,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season,holiday_with_supplies|
     puts "#{season.capitalize}:"
-    holiday_with_supplies.each do
+    holiday_with_supplies.each do |holiday,suppllies|
 end
 
 def all_holidays_with_bbq(holiday_hash)
