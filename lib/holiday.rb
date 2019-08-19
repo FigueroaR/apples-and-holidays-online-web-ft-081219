a28a8e6538ea9cef37085662098ea2e4be6fdc33
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-19 22:02:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,7 +53,8 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash[]
+  holiday_hash.each do |season,supplies|
+    puts "#{season}"
 end
 
 def all_holidays_with_bbq(holiday_hash)
