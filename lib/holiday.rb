a2f8d0d6ffdfe1ae03f4b5a634637f266a79f667
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-19 21:43:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -45,7 +45,7 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash.each do |season, holiday_name|
       if season == :winter
         holiday_name.each do |holiday, supplies|
-        puts supplies
+        puts su
       end
     end
   end
