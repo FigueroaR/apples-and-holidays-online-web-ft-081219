0a87431bfd6a95ac281a1fa709ea10cd7942a370
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-19 21:24:59 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -12,7 +12,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash.each do |season, holiday_name_hash|
     if season == :winter
-      holiday_hash.each do |holiday_name,supplies|
+      holiday_name_hash.each do |holiday_name,supplies|
         supplies << supply 
       end
     end
