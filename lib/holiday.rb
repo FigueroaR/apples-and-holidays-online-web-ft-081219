3ef528f2d58fe33cce89d1cc23a4913a1bea8be3
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-19 22:16:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,6 +72,7 @@ def all_holidays_with_bbq(holiday_hash)
   bbq_array = []
   holiday_hash.each do |season,holiday_supplies_hash|
     holiday_supplies_hash.each do |holiday,supply|
+      if supply.include?("BBQ")
 end
 
 
